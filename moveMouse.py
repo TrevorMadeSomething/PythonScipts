3f5438e27f811b3e32cf679bbbf8ac9b00e740cd
TrevorMadeSomething <54298052+TrevorMadeSomething@users.noreply.github.com>
2020-09-12 20:23:29 -0400

    Add files via upload
    Deletes friends from "League of Legends" client via simple mouse movements with delays.

---
+++ moveMouse.py
@@ -0,0 +1,67 @@
+import pynput
+import time
+import threading
+
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+start_stop_key = KeyCode(char='s')
+exit_key = KeyCode(char='e')
+delay = .3
+button = Button.left
+
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                mouse.position = (1419, 264)
+                time.sleep(.3)
+                mouse.click(Button.right, 1)
+                time.sleep(.3)
+                mouse.position = (1453, 552)
+                time.sleep(.3)
+                mouse.click(Button.left, 1)
+                time.sleep(.3)
+                mouse.position = (735, 530)
+                time.sleep(.3)
+                mouse.click(Button.left, 1)
+                time.sleep(.3)
+            time.sleep(.3)
+
+
+mouse = Controller()
+click_thread = ClickMouse(delay, button)
+click_thread.start()
+
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
